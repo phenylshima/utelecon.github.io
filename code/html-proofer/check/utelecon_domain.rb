318eb7301154b1c20f92fd472e498d0069bb352a
femshima <49227365+femshima@users.noreply.github.com>
2022-06-16 10:08:00 +0900

    add `utelecon.github.io` to utelecon domain

--- code/html-proofer/check/utelecon_domain.rb
+++ code/html-proofer/check/utelecon_domain.rb
@@ -4,12 +4,17 @@ require 'html_proofer'
 
 # warn urls which include telecon's domain(utelecon.adm.u-tokyo.ac.jp)
 class UteleconDomain < HTMLProofer::Check
+  UTELECON = [
+    /utelecon\.adm.u-tokyo\.ac\.jp/,
+    /utelecon\.github\.io/
+  ].freeze
+
   def run
     @html.css('a, link, source').each do |node|
       @link = create_element(node)
       next if @link.ignore?
 
-      if %r{https:?//utelecon\.adm\.u-tokyo\.ac\.jp/}.match?(@link.link_attribute)
+      if UTELECON.any? { |domain| domain.match?(@link.link_attribute) }
         add_failure("linking to #{@link.link_attribute}, which includes utelecon's domain(use relative or absolute path instead).")
       end
     end
