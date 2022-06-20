58130d754f8b8205ed875f7482de7b5c4633fe3d
femshima <49227365+femshima@users.noreply.github.com>
2022-06-20 16:42:40 +0900

    export annotations

--- code/html-proofer/run.rb
+++ code/html-proofer/run.rb
@@ -6,6 +6,11 @@ require 'fileutils'
 require_relative 'check/utelecon_domain'
 
 class CustomRunner < HTMLProofer::Runner
+  def initialize(src, opts)
+    super
+    @path_dict = {}
+  end
+
   def report_failed_checks; end
 
   def check_parsed(path, source)
@@ -14,10 +19,11 @@ class CustomRunner < HTMLProofer::Runner
       text = node.text.strip
       next unless text.start_with?('html-proofer:')
 
-      parse = /^html-proofer:(?<bool>true|false)$/.match(text)
+      parse = /^html-proofer:\{check:(?<bool>true|false),path:"(?<path>.*)"\}$/.match(text)
       next if parse.nil? || !parse.names.include?('bool')
 
       should_check = false if parse[:bool] != 'true'
+      @path_dict[path] = parse[:path] if parse.names.include?('path') && !path.empty?
 
       break
     end
@@ -60,4 +66,9 @@ end
 File.open('_report/external.json', 'w') do |file|
   external = proofer.instance_variable_get('@external_urls')
   file.write(JSON[external])
+end
+
+File.open('_report/path.json', 'w') do |file|
+  path_dict = proofer.instance_variable_get('@path_dict')
+  file.write(JSON[path_dict])
 end
